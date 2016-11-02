969b14b4058d257946830cc2a90d8fb401168766
Jeff Wainwright <jjwainwright2@gmail.com>
2016-11-02 00:56:53 -0700

    adds initial setup for eslinting

--- src/stickybits.js
+++ src/stickybits.js
@@ -3,22 +3,22 @@ export default function stickybit(target, opts) {
   if (!('length' in els)) els = [els];
 
   const defaults = {
-		classname: 'js-stickybit',
-		position: 0,
-		starts: 0,
-		stops: 0,
+    classname: 'js-stickybit',
+    position: 0,
+    starts: 0,
+    stops: 0,
     stuck: 'top',
   };
-	const classname = opts && opts.classname || defaults.classname;
-	const position = opts && opts.position || defaults.position;
-	const starts = opts && opts.starts || defaults.starts;
-	const stops = opts && opts.stops || defaults.stops;
+  const classname = opts && opts.classname || defaults.classname;
+  const position = opts && opts.position || defaults.position;
+  const starts = opts && opts.starts || defaults.starts;
+  const stops = opts && opts.stops || defaults.stops;
   const stuck = opts && opts.stuck || defaults.stuck;
 
   for (let i = 0; i < els.length; i++) {
     const el = els[i];
-		// stuff
-	}
+    // stuff
+  }
 }
 
 const plugin = window.$ || window.jQuery || window.Zepto;
