3483dc77451a52c0d779de96ac2f4ebb48e75ed2
Jeff Wainwright <jjwainwright2@gmail.com>
2017-06-06 00:00:03 -0700

    updates modules in `package.json`

--- dist/stickybits.js
+++ dist/stickybits.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global.stickybits = factory());
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.stickybits = factory());
 }(this, (function () { 'use strict';
 
 var browserPrefix = ['', '-o-', '-webkit-', '-moz-', '-ms-'];
