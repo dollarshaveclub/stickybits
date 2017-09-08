588e1d9b1626ffc7ef632e5df9f9b17b1c2ee758
Jeff Wainwright <yowainwright@gmail.com>
2017-09-07 19:42:32 -0700

    [update] adds initial stickybit jest unit tests

--- tests/unit/test.stickybits.js
+++ tests/unit/test.stickybits.js
@@ -1,8 +1,13 @@
-const ManageSticky = require('../../src/manageSticky.js')
 const stickybits = require('../../src/stickybits.js')
 
-test('Jest is working', () => {
-  console.log('here')
-  // jest is working
-  expect(1).toBe(1)
-});
+test('Jest is working', () => expect(1).toBe(1))
+
+test('Jest paths are pointed correctly, dom is ready to go', () => {
+  // Set up our document body
+  document.body.innerHTML = '<div id="stickybit"></div>'
+  const div = document.getElementById('stickybit')
+  const stickybit = stickybits
+  // test the element
+  expect(div.id).toBe('stickybit')
+  expect(typeof stickybits).toBe('object')
+})
