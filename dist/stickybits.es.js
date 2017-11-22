35baa4a1952891673fa23ca12bf8465d1db2cc66
Zablotskiy <serhiyzablotskiyweb@gmail.com>
2017-11-22 12:12:44 +0200

    Fix `stickyBitStickyOffset` and `useStickyClasses` both usage

--- dist/stickybits.es.js
+++ dist/stickybits.es.js
@@ -192,17 +192,18 @@ Stickybits.prototype.getClosestParent = function getClosestParent(el, matchSelec
 */
 Stickybits.prototype.computeScrollOffsets = function computeScrollOffsets(item) {
   var it = item;
-  var p = it.props;
-  var parent = it.parent;
-  var iw = it.isWin;
+  var p = it.props,
+      parent = it.parent,
+      iw = it.isWin;
+
   var scrollElOffset = 0;
   var stickyStart = parent.getBoundingClientRect().top;
   if (!iw && p.positionVal === 'fixed') {
     scrollElOffset = p.scrollEl.getBoundingClientRect().top;
     stickyStart = parent.getBoundingClientRect().top - scrollElOffset;
   }
-  it.offset = scrollElOffset + p.stickyBitStickyOffset;
-  it.stickyStart = stickyStart;
+  it.offset = scrollElOffset;
+  it.stickyStart = stickyStart - p.stickyBitStickyOffset;
   it.stickyStop = it.stickyStart + parent.offsetHeight - (it.el.offsetHeight - it.offset);
   return it;
 };
@@ -234,11 +235,12 @@ Stickybits.prototype.toggleClasses = function toggleClasses(el, r, a) {
 Stickybits.prototype.manageState = function manageState(item) {
   // cache object
   var it = item;
-  var e = it.el;
-  var p = it.props;
-  var state = it.state;
-  var start = it.stickyStart;
-  var stop = it.stickyStop;
+  var e = it.el,
+      p = it.props,
+      stop = it.stickyStop,
+      start = it.stickyStart,
+      state = it.state;
+
   var stl = e.style;
   // cache props
   var ns = p.noStyles;
--- src/stickybits.js
+++ src/stickybits.js
@@ -189,17 +189,15 @@ Stickybits.prototype.getClosestParent = function getClosestParent(el, matchSelec
 */
 Stickybits.prototype.computeScrollOffsets = function computeScrollOffsets(item) {
   const it = item
-  const p = it.props
-  const parent = it.parent
-  const iw = it.isWin
+  const { props: p, parent, isWin: iw } = it
   let scrollElOffset = 0
   let stickyStart = parent.getBoundingClientRect().top
   if (!iw && p.positionVal === 'fixed') {
     scrollElOffset = p.scrollEl.getBoundingClientRect().top
     stickyStart = parent.getBoundingClientRect().top - scrollElOffset
   }
-  it.offset = scrollElOffset + p.stickyBitStickyOffset
-  it.stickyStart = stickyStart
+  it.offset = scrollElOffset
+  it.stickyStart = stickyStart - p.stickyBitStickyOffset
   it.stickyStop = (it.stickyStart + parent.offsetHeight) - (it.el.offsetHeight - it.offset)
   return it
 }
@@ -231,11 +229,13 @@ Stickybits.prototype.toggleClasses = function toggleClasses(el, r, a) {
 Stickybits.prototype.manageState = function manageState(item) {
   // cache object
   const it = item
-  const e = it.el
-  const p = it.props
-  const state = it.state
-  const start = it.stickyStart
-  const stop = it.stickyStop
+  const {
+    el: e,
+    props: p,
+    stickyStop: stop,
+    stickyStart: start,
+    state,
+  } = it
   const stl = e.style
   // cache props
   const ns = p.noStyles
