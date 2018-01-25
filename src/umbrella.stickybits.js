82054dae584d573a3430395c1b6b322d7bd90f35
Daniel Ruf <daniel@daniel-ruf.de>
2018-01-26 00:30:19 +0100

    add Umbrella JS plugin, fixes #208

---
+++ src/umbrella.stickybits.js
@@ -0,0 +1,10 @@
+import stickybits from './stickybits'
+
+if (typeof window !== 'undefined') {
+  const plugin = window.u
+  if (plugin) {
+    plugin.prototype.stickybits = function stickybitsPlugin(opts) {
+      stickybits(this, opts)
+    }
+  }
+}
