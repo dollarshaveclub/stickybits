1d02a610e8e0f26adf11c8c9cf1c123cab201663
Jeff Wainwright <yowainwright@gmail.com>
2018-01-29 22:24:46 -0800

    [update] adds bundle configs

---
+++ configs/config.js
@@ -0,0 +1,42 @@
+import {
+  author,
+  description,
+  homepage,
+  license,
+  name,
+  version
+} from '../package.json'
+
+const babelSetup = {
+  babelrc: false,
+  presets: [['es2015', { modules: false }]],
+  exclude: 'node_modules/**'
+}
+
+const uglifyOutput = {
+  output: {
+    comments: function (node, comment) {
+      const text = comment.value
+      const type = comment.type
+      if (type === 'comment2') {
+        // multiline comment
+        return /@preserve|@license|@cc_on/i.test(text)
+      }
+    }
+  }
+}
+
+const banner = `/**
+  ${name} - ${description}
+  @version v${version}
+  @link ${homepage}
+  @author ${author}
+  @license ${license}
+**/`
+
+export {
+  babelSetup,
+  banner,
+  uglifyOutput,
+  version
+}
