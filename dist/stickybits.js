3642281f03f6975361c7e1cd91edc49325ee4e11
Jeff Wainwright <yowainwright@gmail.com>
2018-04-10 15:16:16 -0700

    [bug] fixes weird compilation error scroll => ternary

--- dist/stickybits.js
+++ dist/stickybits.js
@@ -1,6 +1,6 @@
 /**
   stickybits - Stickybits is a lightweight alternative to `position: sticky` polyfills
-  @version v3.2.2
+  @version v3.2.3
   @link https://github.com/dollarshaveclub/stickybits#readme
   @author Jeff Wainwright <yowainwright@gmail.com> (https://jeffry.in)
   @license MIT
@@ -65,7 +65,7 @@
   */
   function Stickybits(target, obj) {
     var o = typeof obj !== 'undefined' ? obj : {};
-    this.version = '3.2.2';
+    this.version = '3.2.3';
     this.userAgent = window.navigator.userAgent || 'no `userAgent` provided by the browser';
     this.props = {
       customStickyChangeNumber: o.customStickyChangeNumber || null,
@@ -297,13 +297,7 @@
     */
 
     var tC = this.toggleClasses;
-
-    if (this.isWin) {
-      var _scroll = window.scrollY || window.pageYOffset;
-    } else {
-      var _scroll2 = se.scrollTop;
-    }
-
+    var scroll = this.isWin ? window.scrollY || window.pageYOffset : se.scrollTop;
     var notSticky = scroll > start && scroll < stop && (state === 'default' || state === 'stuck');
     var isSticky = scroll <= start && state === 'sticky';
     var isStuck = scroll >= stop && state === 'sticky';
