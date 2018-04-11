cf695d6cf1999e87062f2a6bc21c4f0dcd7f08ce
Tony Valderrama <406656+tvald@users.noreply.github.com>
2018-04-11 14:12:19 +0900

    Add typings. (#273)

---
+++ types/index.d.ts
@@ -0,0 +1,23 @@
+export default function stickybits(
+  target: string | Element | Element[],
+  options?: StickyBits.Options,
+): StickyBits
+
+export interface StickyBits {
+  cleanup: () => void
+}
+
+export namespace StickyBits {
+  export interface Options {
+    customStickyChangeNumber?: number | null
+    noStyles?: boolean
+    stickyBitStickyOffset?: number
+    parentClass?: string
+    scrollEl?: Element
+    stickyClass?: string
+    stuckClass?: string
+    stickyChangeClass?: string
+    useStickyClasses?: boolean
+    verticalPosition?: 'top' | 'bottom'
+  }
+}
