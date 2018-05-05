7dc63a6d783e64dc865f9f942cccad42037bc990
Jeff Wainwright <yowainwright@gmail.com>
2018-05-05 13:08:02 -0700

    [chore] version bump/test => getOffsetTop (#298)

--- tests/unit/test.stickybits.js
+++ tests/unit/test.stickybits.js
@@ -112,6 +112,16 @@ test('stickybits .getClosestParent interface', () => {
   expect(parent.id).toBe('parent')
 })
 
+test('stickybits .getOffsetTop interface', () => {
+  // Set up our document
+  document.body.innerHTML = '<div id="parent"><div><div id="child"><div id="manage-sticky"></div></div></div></div>'
+  const child = document.getElementById('child')
+  const parentEl = document.getElementById('parent')
+  const stickybit = stickybits('#manage-sticky')
+  const parentOffsetTop = stickybit.getOffsetTop(parentEl)
+  expect(parentOffsetTop).toBe(0)
+})
+
 test('stickybits .computeScrollOffsets interface', () => {
   // Set up our document
   document.body.innerHTML = '<div id="parent"><div id="manage-sticky"></div></div>'
