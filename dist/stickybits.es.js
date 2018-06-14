ca6498cff931d8dd55ff5ab463f3e65c61be8350
Jeff Wainwright <yowainwright@gmail.com>
2018-06-14 13:42:49 -0700

    [init] adds initial useFixed README docs (#325)

--- dist/stickybits.es.js
+++ dist/stickybits.es.js
@@ -1,6 +1,6 @@
 /**
   stickybits - Stickybits is a lightweight alternative to `position: sticky` polyfills
-  @version v3.3.3
+  @version v3.3.5
   @link https://github.com/dollarshaveclub/stickybits#readme
   @author Jeff Wainwright <yowainwright@gmail.com> (https://jeffry.in)
   @license MIT
@@ -64,7 +64,7 @@ var Stickybits =
 function () {
   function Stickybits(target, obj) {
     var o = typeof obj !== 'undefined' ? obj : {};
-    this.version = '3.3.3';
+    this.version = '3.3.5';
     this.userAgent = window.navigator.userAgent || 'no `userAgent` provided by the browser';
     this.props = {
       customStickyChangeNumber: o.customStickyChangeNumber || null,
