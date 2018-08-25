ef62380e7271ac41f7d12ca2b9396fdfc82a8e1e
Jeff Wainwright <yowainwright@gmail.com>
2018-08-24 19:27:33 -0700

    [update] clean up for loop

--- page/app.js
+++ page/app.js
@@ -5,10 +5,9 @@
   if (window.innerWidth < 900) return;
   var wrapper = document.getElementById('wrapper');
   wrapper.className += ' js-has-aside';
-  var stickyItems = ['1', '2', '3', '4'];
   var stickyWrapper = document.getElementById('aside');
 
-  for (var i = 0; i < stickyItems.length; i += 1) {
+  for (var i = 1; i < 5; i += 1) {
     var stickyItem = '<div class="sticky-element sticky-element--' + i + '">StickyBit Item ' + i + '<span class="stuck">is Stuck! 🔥</span></div>';
     var stickyParent = '<div class="sticky-parent sticky-parent--' + i + '">' + stickyItem + '</div>';
     stickyWrapper.innerHTML += stickyParent;
